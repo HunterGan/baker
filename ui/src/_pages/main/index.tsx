19582e5d91b30cc6a6da744f516a317692c8d89b
hunter <n.ohotnikov@collabox.dev>
2024-04-05 16:56:09 +0300

    sync

--- ui/src/_pages/main/index.tsx
+++ ui/src/_pages/main/index.tsx
@@ -3,6 +3,8 @@ import Box from "@mui/material/Box";
 import Slider from "@/_app/routes/layouts/ui/components/Slider";
 import DeliveryConditions from "@/_app/routes/layouts/ui/components/DeliveryConditions";
 import CategoriesLinks from "@/_app/routes/layouts/ui/components/CategoriesLinks";
+import Typography from "@mui/material/Typography";
+import styles from "@/_shared/lib/styles";
 
 const MainPage: FC = () => {
   return (
@@ -17,6 +19,48 @@ const MainPage: FC = () => {
         <Slider/>
         <DeliveryConditions/>
         <CategoriesLinks/>
+        {/* goods */}
+
+        <Box
+          sx={{
+            display: 'flex',
+            padding: '25px',
+            gap: '35px',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+        >
+          <Box
+            sx={{
+              height: 114,
+              width: 175,
+              backgroundImage: `url('/logo_large.png')`,
+              backgroundSize: 'contain',
+              backgroundRepeat: 'no-repeat',
+            }}
+          />
+          <Box
+            sx={{
+              width: '2px',
+              heigth: '100%',
+              backgroundColor: '#694D3f',
+              opacity: 0.6
+            }}
+          />
+          <Typography
+            sx={{
+              fontSize: '20px',
+              color: styles.colors.text_primary,
+              lineHeight: '100%',
+            }}
+          >
+            Мы сеть предприятий общественного питания! В наш состав входят:\n
+            2 столовые, пекарня, и 4 буфета. Мы готовим для вас:\n
+            - Без вредных добавок
+            - Только из свежих продуктов
+            - Разнообразные блюда каждый день
+          </Typography>
+        </Box>
       </Box>
     </>
   )
--- ui/src/_shared/ui/common/StyledLink.tsx
+++ ui/src/_shared/ui/common/StyledLink.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { Typography } from "@mui/material"
-import Link from "next/link"
 import { FC, ReactNode } from "react"
-import styles from "@/_shared/lib/styles";
+import Link from "next/link"
 import { usePathname } from "next/navigation";
+import Typography from "@mui/material/Typography"
+import styles from "@/_shared/lib/styles";
 
 interface IStyledLink {
   href: string
